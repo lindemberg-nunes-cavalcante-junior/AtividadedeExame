f60aa43094b79151c92549ac846a633bd3e28775
LINDEMBERG NUNES CAVALCANTE JUNIOR <lindemberg.j@academico.ifrn.edu.br>
2022-11-01 12:00:17 -0300

    Adicionar arquivos de projeto.

---
+++ Modelo/Exame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Exame
+    {
+        public long Id { get; set; }
+        public string descrição { get; set; }
+    }
+}
---
+++ Persistencia/Context/EFContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Persistencia.Context
+{
+    class EFContext: DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+            new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+        public DbSet<Exame> Exames { get; set; }
+    }
+}
